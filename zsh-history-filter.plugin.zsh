cf507425c0bcd7d9668fe911d5364f155c17e620
Michael Aquilina <michaelaquilina@gmail.com>
2019-01-03 17:45:00 +0000

    Add rewrite_history command

--- zsh-history-filter.plugin.zsh
+++ zsh-history-filter.plugin.zsh
@@ -1,16 +1,43 @@
-export HISTORY_FILTER_VERSION="0.3.0"
+export HISTORY_FILTER_VERSION="0.2.0"
 
-function _history_filter() {
-    for value in $HISTORY_FILTER_EXCLUDE; do
-        if [[ "$1" = *"$value"* ]]; then
-            if [[ -z "$HISTORY_FILTER_SILENT" ]]; then
-                (>&2 printf "Excluding command from history\n")
-            fi
-            return 2
+# overwrite the history file so that it
+# retro-actively applies the currently set filters
+function rewrite_history() {
+    local new_history="$HISTFILE.bak"
+    local excluded=0
+
+    cat $HISTFILE | while read entry; do
+        if ! _matches_filter "$entry"; then
+            echo "$entry" >> "$new_history"
+        else
+            ((excluded = excluded+ 1))
+            printf "\rExcluded $excluded entries"
         fi
     done
+    printf "\n"
+    mv "$new_history" "$HISTFILE"
+}
 
-    return 0
+
+function _matches_filter() {
+    for value in $HISTORY_FILTER_EXCLUDE; do
+        if [[ "$1" = *"$value"* ]]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
+
+function _history_filter() {
+    if _matches_filter "$1"; then
+        if [[ -z "$HISTORY_FILTER_SILENT" ]]; then
+            (>&2 printf "Excluding command from history\n")
+        fi
+        return 2
+    else
+        return 0
+    fi
 }
 
 autoload -Uz add-zsh-hook
