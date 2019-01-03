9dc27cbbc1e7555b514164b234cc6466d3c7e9ab
Michael Aquilina <michaelaquilina@gmail.com>
2019-01-03 15:37:19 +0000

    Numerous fixes

--- zsh-history-filter.plugin.zsh
+++ zsh-history-filter.plugin.zsh
@@ -1,13 +1,17 @@
 export HISTORY_FILTER_VERSION="0.1.0"
 
-function zshaddhistory() {
-    for value in $HISTORY_EXCLUDE; do
+function _filter_history() {
+    for value in $HISTORY_FILTER_EXCLUDE; do
         if [[ "$1" = *"$value"* ]]; then
-            if [[ -n "$HISTORY_FILTER_DEBUG" ]]; then
+            if [[ -z "$HISTORY_FILTER_SILENT" ]]; then
                 printf "Excluding result from history\n"
             fi
             return 2
         fi
     done
+
     return 0
 }
+
+autoload -Uz add-zsh-hook
+add-zsh-hook zshaddhistory _filter_history
