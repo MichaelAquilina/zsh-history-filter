b0f5c291cdead3b34b9fde31769287efa7bb8bcf
Joe Schaefer <joe@sunstarsys.com>
2021-10-13 10:55:39 -0400

    Update zsh-history-filter.plugin.zsh

--- zsh-history-filter.plugin.zsh
+++ zsh-history-filter.plugin.zsh
@@ -37,7 +37,7 @@ function _history_filter() {
     if _matches_filter "$1"; then
         if [[ -z "$HISTORY_FILTER_SILENT" ]]; then
             (>&2 printf "Excluding command from history\n")
-            ptyoff
+            echo pty-driver "off"
         fi
         return 2
     else
