3f01419e1c6c15195640913e1ceda534578318f0
Joe Schaefer <joe@sunstarsys.com>
2021-10-13 18:29:07 -0400

    Update zsh-history-filter.plugin.zsh

--- zsh-history-filter.plugin.zsh
+++ zsh-history-filter.plugin.zsh
@@ -1,4 +1,4 @@
-export HISTORY_FILTER_VERSION="0.4.2-joesuf4"
+export HISTORY_FILTER_VERSION="0.4.3-joesuf4"
 
 # overwrite the history file so that it
 # retro-actively applies the currently set filters
